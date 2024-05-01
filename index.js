c5acabed96ae10ec915f40ced23f898074f3e81f
mahammadshariaralamsarker <154776315+mahammadshariaralamsarker@users.noreply.github.com>
2024-05-01 23:13:33 +0600

    update read

--- index.js
+++ index.js
@@ -2,8 +2,8 @@
 const express = require('express');
 const cors = require('cors');
 const app = express();
-const port = process.env.PORT||5000;
-const { MongoClient, ServerApiVersion } = require('mongodb');
+const port = process.env.PORT || 5000;
+const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
 require('dotenv').config()
 console.log(process.env.DB_PASS)
 console.log(process.env.DB_USER)
@@ -30,22 +30,47 @@ async function run() {
   try {
     // Connect the client to the server	(optional starting in v4.7)
     await client.connect();
+
+    const serviceCollections = client.db('car_doctor').collection("services");
+
+    app.get('/services', async (req, res) => {
+      const cursor = serviceCollections.find();
+      const result = await cursor.toArray();
+      res.send(result)
+    })
+
+    app.get('/services/:id', async (req, res) => {
+      const id = req.params.id;
+      const query = { _id: new ObjectId(id) }
+      const options = {
+        projection: { _id: 1, title: 1, price: 1,service_id:1, },
+      };
+      const result = await serviceCollections.findOne(query,options)
+      res.send(result)
+    })
+
+
+
+
+
+
+
     // Send a ping to confirm a successful connection
     await client.db("admin").command({ ping: 1 });
     console.log("Pinged your deployment. You successfully connected to MongoDB!");
   } finally {
     // Ensures that the client will close when you finish/error
-    await client.close();
+    // await client.close();
   }
 }
 run().catch(console.dir);
 
 
 
-app.get('/',(req,res)=>{
-    res.send('simple crud is running ')
+app.get('/', (req, res) => {
+  res.send('simple crud is running ')
 })
 
-app.listen(port,()=>{
-    console.log(`runnging port ${port}`)
+app.listen(port, () => {
+  console.log(`runnging port ${port}`)
 })
